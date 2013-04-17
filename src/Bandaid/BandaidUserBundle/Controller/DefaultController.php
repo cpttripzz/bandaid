5779a1572d4eaee06b53843fff21ae994bae482f
Zachary Erskine <zach.erskine@gmail.com>
2013-04-18 00:08:45 +0300

    Add FOSUser Bundle

---
+++ src/Bandaid/BandaidUserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bandaid\BandaidUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BandaidBandaidUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
