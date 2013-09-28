cf366a2a162ce01b581069f1ce8725401e470729
Zachary Erskine <zach.erskine@gmail.com>
2013-09-28 12:20:45 +0300

    add bootstrap, knpmenu bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,11 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-//            new Acme\DemoBundle\Us
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
             new Bandaid\BandaidUserBundle\BandaidBandaidUserBundle(),
         );
 
