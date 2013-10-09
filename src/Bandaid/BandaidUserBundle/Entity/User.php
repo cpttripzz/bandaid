e79b7ab4592cafc62d6a222b21b90e9ff7b371b8
Zachary Erskine <zach.erskine@gmail.com>
2013-10-09 19:46:03 +0200

    conf

--- src/Bandaid/BandaidUserBundle/Entity/User.php
+++ src/Bandaid/BandaidUserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 
     namespace Bandaid\BandaidUserBundle\Entity;
 
-    use FOS\UserBundle\Entity\User as BaseUser;
+    use FOS\UserBundle\Model\User as BaseUser;
     use Doctrine\ORM\Mapping as ORM;
 
     /**
