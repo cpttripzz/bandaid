78fe8344456592a319eb4d9b38cc1eb084d38898
cpttripzz <zach.erskine@gmail.com>
2013-12-08 01:38:21 +0200

    add behat, sonata and other stuff

---
+++ src/Bandaid/BandaidUserBundle/Controller/EntityAdminController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zach
+ * Date: 12/7/13
+ * Time: 8:29 PM
+ */
+namespace Bandaid\BandaidUserBundle\Controller;
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class EntityAdminController extends Controller
+{
+
+}
