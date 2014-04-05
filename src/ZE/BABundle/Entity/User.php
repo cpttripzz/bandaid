1f89947ad05664ab7acc7ea3ee92af96dca1b9b1
cpttripzz <zach.erskine@gmail.com>
2014-04-05 12:30:02 +0300

    add custom handerl oauth, partially working

--- src/ZE/BABundle/Entity/User.php
+++ src/ZE/BABundle/Entity/User.php
@@ -77,9 +77,22 @@ class User extends BaseUser
      *
      * @ORM\Column(name="github_id", type="string", nullable=true)
      */
-    private $githubID;
+    protected $githubID;
+    /** @ORM\Column(name="github_access_token", type="string", length=255, nullable=true) */
 
+    protected $github_access_token;
 
+    /** @ORM\Column(name="facebook_id", type="string", length=255, nullable=true) */
+    protected $facebook_id;
+
+    /** @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true) */
+    protected $facebook_access_token;
+
+    /** @ORM\Column(name="google_id", type="string", length=255, nullable=true) */
+    protected $google_id;
+
+    /** @ORM\Column(name="google_access_token", type="string", length=255, nullable=true) */
+    protected $google_access_token;
     /**
      * Set city
      *
@@ -159,6 +172,125 @@ class User extends BaseUser
     public function getGithubID()
     {
         return $this->githubID;
+    }
+
+
+
+    /**
+     * Set github_access_token
+     *
+     * @param string $githubAccessToken
+     * @return User
+     */
+    public function setGithubAccessToken($githubAccessToken)
+    {
+        $this->github_access_token = $githubAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get github_access_token
+     *
+     * @return string 
+     */
+    public function getGithubAccessToken()
+    {
+        return $this->github_access_token;
+    }
+
+
+
+    /**
+     * Set facebook_id
+     *
+     * @param string $facebookId
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebook_id = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * Get facebook_id
+     *
+     * @return string 
+     */
+    public function getFacebookId()
+    {
+        return $this->facebook_id;
+    }
+
+    /**
+     * Set facebook_access_token
+     *
+     * @param string $facebookAccessToken
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebook_access_token = $facebookAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get facebook_access_token
+     *
+     * @return string 
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebook_access_token;
+    }
+
+    /**
+     * Set google_id
+     *
+     * @param string $googleId
+     * @return User
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->google_id = $googleId;
+
+        return $this;
+    }
+
+    /**
+     * Get google_id
+     *
+     * @return string 
+     */
+    public function getGoogleId()
+    {
+        return $this->google_id;
+    }
+
+    /**
+     * Set google_access_token
+     *
+     * @param string $googleAccessToken
+     * @return User
+     */
+    public function setGoogleAccessToken($googleAccessToken)
+    {
+        $this->google_access_token = $googleAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get google_access_token
+     *
+     * @return string 
+     */
+    public function getGoogleAccessToken()
+    {
+        return $this->google_access_token;
     }
 
 
