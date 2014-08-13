069a661784f4d530424776ae4952c15c45407126
cpttripzz <zach.erskine@gmail.com>
2014-08-14 00:40:18 +0300

    fix 2.x many-to-many association class for band vacancy, looks bad, time to refactor to angularjs

---
+++ src/ZE/BABundle/Form/BandVacancyAssociationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ZE\BABundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BandVacancyAssociationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('bandVacancies')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ZE\BABundle\Entity\BandVacancyAssociation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ze_babundle_bandvacancyassociation';
+    }
+}
