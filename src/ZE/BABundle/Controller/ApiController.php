1bace1d8f2633834668942bb288e0e55abb43b12
cpttripzz <zach.erskine@gmail.com>
2014-08-23 23:57:23 +0300

    fix band address js

--- src/ZE/BABundle/Controller/ApiController.php
+++ src/ZE/BABundle/Controller/ApiController.php
@@ -151,7 +151,7 @@ class ApiController extends Controller
         $address = $this->get('ze.location_manager_service')->saveNewAddress($associationId, $associationType, $address);
         if($address){
 //            $addresses = $this->get('ze.location_manager_service')->getAllAddressesForAssociation($associationId, $associationType);
-            $returnObj = array('callback'=>'reloadAddresses','target' => 's2id_ze_babundle_musician_addresses', 'data' => $address);
+            $returnObj = array('callback'=>'reloadAddresses','target' => 's2id_ze_babundle_'.$associationType.'_addresses', 'data' => $address);
             return new JsonResponse(array("success" => true, "msg" => 'Address created', 'callback' => $returnObj));
         } else{
             return new JsonResponse(array("success" => false, "msg" => 'Address not created'));
