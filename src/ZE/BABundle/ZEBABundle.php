7df58cbff19ce65528acf3768c28a62086cbc29e
cpttripzz <zach.erskine@gmail.com>
2014-12-12 18:16:38 +0200

    jwt auth fixed

--- src/ZE/BABundle/ZEBABundle.php
+++ src/ZE/BABundle/ZEBABundle.php
@@ -6,15 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ZEBABundle extends Bundle
 {
-//    public function boot()
-//    {
-//        // implement alias XR for base namespace
-//        $em = $this->container->get("doctrine.orm.entity_manager");
-//        $config = $em->getConfiguration();
-//        $config->addEntityNamespace("ZEBA", "ZE\\BABundle\\Entity\\Address");
-//    }
-    public function getParent()
-    {
-        return 'FOSUserBundle';
-    }
 }
